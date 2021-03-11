5f62b812f40bc1dada61ebfbf1b6dfd7bd2ad12c
GUISLOTTI <gabrielguislotti@hotmail.com>
2021-03-10 21:07:56 -0300

    primeiro commit

---
+++ projeto1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+                
+namespace projeto1
+{                   
+    class Program
+    {                                                               
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma letra");
+            var tecla = Console.ReadLine();
+            var cachorro = new Cachorro();
+            var resultado = cachorro.Piruetar(tecla);
+            Console.WriteLine(resultado);
+
+        }
+    }        
+    public class Cachorro 
+    {
+        public string Piruetar(string tecla)
+        {
+            if(tecla.ToLower() == "p")
+            {
+                return "Piruetando";
+            }
+            else
+            {
+                return "Comando Inválido";
+
+              
+            }     
+        }
+
+
+
+    }
+}                                                                                                                                                                                                                                
